47ce0bc56b8ad5d1df64f146857049603bf3c986
Niranjan Rajkumar <128575600+Niranjanr0682@users.noreply.github.com>
2023-08-08 20:20:22 +0530

    prog

---
+++ Program/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    int n;
+    printf("Enter n value : ");
+    scanf("%d", &n);
+    printf("Number series from 1 to %d: \n", n);
+    for (int i = 1; i <= n; i++) 
+        {printf("%d ", i);}
+    return 0;
+}
